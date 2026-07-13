b8dcc7612b3c5f1ee899a10f4ae281f19b9ff9c6
Suyine <vivvertieuh@gmail.com>
2026-07-13 21:16:09 +0200

    path

--- reader-web/vite.config.ts
+++ reader-web/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // можливо знадобиться імпорт path
+import path from 'path'; 
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
-      // Це змушує Vite використовувати один і той самий React для всього проєкту
       react: path.resolve('./node_modules/react'),
       'react-dom': path.resolve('./node_modules/react-dom'),
     },
